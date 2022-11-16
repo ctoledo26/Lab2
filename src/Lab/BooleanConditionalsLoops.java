3e29e4077f2b7fd91d7015db0ca3c7bff22fc38b
ctoledo26 <coritoledo@gmail.com>
2022-11-16 14:28:27 -0500

    first commit

---
+++ src/Lab/BooleanConditionalsLoops.java
@@ -0,0 +1,21 @@
+package Lab;
+
+public class BooleanConditionalsLoops {
+
+	public static void main(String[]args) {
+		
+		int age = 50;
+		boolean hasLicense = false;
+		
+		if ( hasLicense = true) {
+			
+			System.out.println("You can drive");
+			
+		}	else {
+			
+			System.out.println("You cannot drive");
+			
+		}
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author coritoledo
+ *
+ */
+module LabPart1 {
+}
